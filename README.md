56fb3454fd60a62a8bb1467c15a0089659d691d1
sergiokas <sergiokas@gmail.com>
2013-08-29 15:34:32 -0300

    base files

---
+++ README.md
@@ -0,0 +1,4 @@
+plugins
+=======
+
+Posting an anonymous GIST in Github from the Code Editor selection
---
+++ src/main/java/com/codenvy/ide/extension/vagrant/VagrantExtensionResources.java
@@ -0,0 +1,20 @@
+package com.codenvy.ide.extension.vagrant;
+
+import com.google.gwt.resources.client.ClientBundle;
+import com.google.gwt.resources.client.CssResource;
+import com.google.gwt.resources.client.ImageResource;
+
+/**
+ * Client resources.
+ */
+public interface VagrantExtensionResources extends ClientBundle {
+    public interface GistCSS extends CssResource {
+        String textFont();
+    }
+
+    @Source({"Gist.css", "com/codenvy/ide/api/ui/style.css"})
+    GistCSS gistCSS();
+
+    @Source("com/codenvy/ide/extension/demo/images/github.png")
+    ImageResource github();
+}
