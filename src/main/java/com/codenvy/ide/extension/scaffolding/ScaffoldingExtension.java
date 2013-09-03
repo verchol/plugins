34a72c352a5149ca58da996df621b22e2d60085c
ide <>
2013-09-03 15:30:51 +0000

    updating entry file to display a console message

--- src/main/java/com/codenvy/ide/extension/scaffolding/ScaffoldingExtension.java
+++ src/main/java/com/codenvy/ide/extension/scaffolding/ScaffoldingExtension.java
@@ -1,8 +1,7 @@
 package com.codenvy.ide.extension.scaffolding;
 
 import com.codenvy.ide.api.extension.Extension;
-import com.codenvy.ide.api.ui.action.ActionManager;
-import com.codenvy.ide.api.ui.action.DefaultActionGroup;
+import com.codenvy.ide.api.parts.ConsolePart;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 
@@ -14,11 +13,11 @@ import com.google.inject.Singleton;
 public class ScaffoldingExtension {
 
     @Inject
-    public ScaffoldingExtension(ActionManager actionManager,
-                         ScaffoldingExtensionLocalizationConstant localizationConstants) {
-        // register a new action
-        // actionManager.registerAction(localizationConstants.createGistActionlId(), createVagrantfileAction);
-        // DefaultActionGroup saveActionGroup = (DefaultActionGroup)actionManager.getAction("saveGroup");
-        // saveActionGroup.add(createVagrantfileAction);
+    public ScaffoldingExtension(
+                         ScaffoldingExtensionLocalizationConstant localizationConstants,
+                         ConsolePart console) {
+        
+        // Show a simple message in the console
+        console.print(localizationConstants.extensionRunning());        
     }
 }
